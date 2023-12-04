c27bf11a1772a8a65a32f76d6fcea8e32cd25ba2
Grzesiek <gromaniuk0@gmail.com>
2023-12-04 14:17:29 +0100

    Refactored to use extra information in modelbuilder

--- MyEFCourse/Entities/MyBoardsContext.cs
+++ MyEFCourse/Entities/MyBoardsContext.cs
@@ -17,6 +17,24 @@ namespace MyEFCourse.Entities
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<WorkItem>()
+                .Property(x => x.State)
+                .IsRequired();
+
+            modelBuilder.Entity<WorkItem>()
+                .Property(x => x.Area)
+                .HasColumnType("varchar(200)");
+
+            modelBuilder.Entity<WorkItem>(eb =>
+            {
+                eb.Property(wi => wi.IterationPath).HasColumnName("My_Iteration_path");
+                eb.Property(wi => wi.Effort).HasColumnType("decimal(5,2)");
+                eb.Property(wi => wi.EndDate).HasPrecision(3);
+                eb.Property(wi => wi.Activity).HasMaxLength(200);
+                eb.Property(wi => wi.RemainingWork).HasPrecision(14,2);
+            });
+            
         }
     }
 }
--- MyEFCourse/Entities/WorkItem.cs
+++ MyEFCourse/Entities/WorkItem.cs
@@ -7,24 +7,17 @@ namespace MyEFCourse.Entities
     public class WorkItem
     {
         public int Id { get; set; }
-        [Required]
         public string State { get; set; }
-        [Column(TypeName = "varchar(200)")]
         public string Area { get; set; }
-        [Column("My_iteration_name")]
         public string IterationPath { get; set; }
         public int Priority { get; set; }
         //Epic
         public DateTime? StartDate { get; set; }
-        [Precision(3)]
         public DateTime? EndDate { get; set;}
         //Issue
-        [Column(TypeName = "double(5,2)")]
         public double Effort { get; set; }
         //Task
-        [MaxLength(100)]
         public string Activity { get; set; }
-        [Precision(14,2)]
         public double RemainingWork { get; set; }
 
         public string Type { get; set; }
