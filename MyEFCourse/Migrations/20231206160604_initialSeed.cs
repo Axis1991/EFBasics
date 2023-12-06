d0d1f04b8435a400dc637a171bbd584e87eda577
Grzesiek <gromaniuk0@gmail.com>
2023-12-06 19:38:25 +0100

    Custom migration

---
+++ MyEFCourse/Migrations/20231206160604_initialSeed.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MyEFCourse.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "States",
+                columns: new[] { "Id", "States" },
+                values: new object[,]
+                {
+                    { 1, "To do" },
+                    { 2, "Doing" },
+                    { 3, "Done" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "States",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "States",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "States",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
---
+++ MyEFCourse/Migrations/20231206161015_AdditionalStatesSeeded.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyEFCourse.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdditionalStatesSeeded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "States",
+                column: "States",
+                value: "On hold");
+            migrationBuilder.InsertData(
+                table: "States",
+                column: "States",
+                value: "Rejected");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+            table: "States",
+            keyColumn: "States",
+            keyValue: "On hold");
+
+            migrationBuilder.DeleteData(
+            table: "States",
+            keyColumn: "States",
+            keyValue: "Rejected");
+        }
+    }
+}
